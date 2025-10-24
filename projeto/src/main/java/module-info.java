1043bd456c90dfcf7d26016437381f02dad4813a
CaioMarinho2 <caionovo10@gmail.com>
2025-10-23 22:04:30 -0300

    cadastro de usuario e conexão com banco de dados prontos e funcionais, faltando apenas algumas verificações para o cadastro

--- projeto/src/main/java/module-info.java
+++ projeto/src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module projeto.lp3 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens projeto.lp3 to javafx.fxml;
+
     exports projeto.lp3;
 }
---
+++ projeto/src/main/java/projeto/lp3/DatabaseConnection.java
@@ -0,0 +1,41 @@
+package projeto.lp3;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseConnection {
+
+    private static final String CONFIG_FILE = "/config.properties";
+
+    private static String URL;
+    private static String USER;
+    private static String PASSWORD;
+
+    static {
+        try (InputStream input = DatabaseConnection.class.getResourceAsStream(CONFIG_FILE)) {
+            Properties props = new Properties();
+            if (input == null) {
+                throw new RuntimeException("Arquivo config.properties não encontrado!");
+            }
+            props.load(input);
+            URL = props.getProperty("db.url");
+            USER = props.getProperty("db.user");
+            PASSWORD = props.getProperty("db.password");
+        } catch (IOException ex) {
+            System.out.println("Erro ao carregar config.properties: " + ex.getMessage());
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar ao banco: " + e.getMessage());
+            return null;
+        }
+    }
+}
